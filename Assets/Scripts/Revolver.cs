b878f8cfedaf31b117decee6a74efc23d6a06cfe
vincentli402 <102388777+vincentli402@users.noreply.github.com>
2024-04-08 18:38:13 -0400

    implemented shooting

---
+++ Assets/Scripts/Revolver.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Revolver : Weapon
+{
+[SerializeField] private Projectile bulletPrefab;
+
+    protected override void StartShooting(XRBaseInteractor interactor)
+    {
+        base.StartShooting(interactor);
+        Shoot();
+    }
+
+    protected override void Shoot()
+    {
+        base.Shoot();
+        Projectile projectileInstance = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        projectileInstance.Init(this);
+        projectileInstance.Launch();
+    }
+
+    protected override void StopShooting(XRBaseInteractor interactor)
+    {
+        base.StopShooting(interactor);
+    }
+}
