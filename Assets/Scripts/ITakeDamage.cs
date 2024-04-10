d246a0c2d0164faa06b6545f53e57ee9a2a31218
vincentli402 <102388777+vincentli402@users.noreply.github.com>
2024-04-10 01:56:03 -0400

    implemented functioning grab and shoot interaction

--- Assets/Scripts/ITakeDamage.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ITakeDamage
-{
-    void TakeDamage(Weapon weapon, Projectile projectile, Vector3 contactPoint);
-}
