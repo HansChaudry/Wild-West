23ce40c860846842f92768dd39b26d3c9363510e
Hans Chaudry <62669434+HansChaudry@users.noreply.github.com>
2024-05-06 22:36:13 -0400

    added bullets remaining to the UI, and reload pack

--- Assets/Scenes/Scene 3/Scripts/Gun.cs
+++ Assets/Scenes/Scene 3/Scripts/Gun.cs
@@ -1,23 +1,49 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
-{
-    [SerializeField] private GameObject bullet;
-    [SerializeField] private Transform bulletPosition;
-    [SerializeField] private float shootDelay = 0.2f;
-    [Range(0, 3000), SerializeField] private float bulletSpeed;
-    [Space, SerializeField] private AudioSource audioSource;
-
+{ 
+    private float shootDelay = 0.2f;
+    private float bulletSpeed = 3000f;
+    private int magazine = 6;
     private float lastShot;
+
+    public TMPro.TMP_Text magazineText;
+    public GameObject bullet;
+    public Transform bulletPosition;
+    public AudioSource audioSource;
     public Animator animator;
 
+    private void Start()
+    {
+        magazineText.text = magazine.ToString();
+    }
 
     public void Shoot()
+    {
+        if (magazine > 0)
+        {
+            fireGun();
+        }
+
+        //flash the reload text
+    }
+
+    private void GunShotAudio()
+    {
+        var random = Random.Range(0.8f, 1.2f);
+        audioSource.pitch = random;
+        audioSource.Play();
+    }
+
+    private void fireGun()
     {
         if (lastShot > Time.time) return;
         lastShot = Time.time + shootDelay;
         animator.SetTrigger("Recoil");
         GunShotAudio();
+        magazine -= 1;
+        magazineText.text = magazine.ToString();
 
         var bulletPrefab = Instantiate(bullet, bulletPosition.position, bulletPosition.rotation);
         var bulletRB = bulletPrefab.GetComponent<Rigidbody>();
@@ -27,10 +53,9 @@ public class NewBehaviourScript : MonoBehaviour
         Destroy(bulletPrefab, 5f);
     }
 
-    private void GunShotAudio()
+    private void reload()
     {
-        var random = Random.Range(0.8f, 1.2f);
-        audioSource.pitch = random;
-        audioSource.Play();
+        magazine = 6;
+        magazineText.text = magazine.ToString();
     }
 }
